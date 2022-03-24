c71b64435662f4b159241b2508ed2e1a848a48c5
kongwoojin <kongwoojin03@gmail.com>
2022-03-24 15:00:53 +0900

    Initial Commit

---
+++ app/src/main/java/com/kongjak/bcsd_lab_beginner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kongjak.bcsd_lab_beginner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
